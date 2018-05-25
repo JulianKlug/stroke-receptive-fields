e539450f356c05181a577ad677fbb1398eaf7af7
monsieurwave <tensu.wave@gmail.com>
2018-05-25 13:34:33 +0200

    Implemented shorter version of receptive field

--- analysis/voxelWise/receptiveField.py
+++ analysis/voxelWise/receptiveField.py
@@ -7,11 +7,67 @@ import numpy as np
 import itertools
 
 def reshape_to_receptive_field(input_data_list, output_data_list, receptive_field_dimensions) :
-    temp = new_reshape_to_receptive_field(input_data_list, output_data_list, receptive_field_dimensions)
+    temp = short_new_reshape_to_receptive_field(input_data_list, output_data_list, receptive_field_dimensions)
+    # a_temp2, b_temp2 = new_reshape_to_receptive_field(input_data_list, output_data_list, receptive_field_dimensions)
+
+    # print('comparing inpout, output', np.array_equal(a_temp, a_temp2), np.array_equal(b_temp, b_temp2))
+
 
     # temp = old_reshape_to_receptive_field(input_data_list, output_data_list, receptive_field_dimensions)
 
     return temp
+
+def short_new_reshape_to_receptive_field(input_data_list, output_data_list, receptive_field_dimensions) :
+    # Dimensions of the receptive field defined as distance to center point in every direction
+    rf_x, rf_y, rf_z = receptive_field_dimensions
+    window_d_x, window_d_y, window_d_z  = 2 * np.array(receptive_field_dimensions) + 1
+    print('Receptive field window dimensions are: ', window_d_x, window_d_y, window_d_z )
+
+    # Declare final array going into model
+    # Initialize the inputs array as long as there will be receptive fields, and on the second dimensions as many voxels as there are in a receptive field
+    n_receptive_fields = input_data_list[0][:, :, :, 0].size * len(input_data_list) # ie voxels per image times number of images
+    receptive_field_size = window_d_x * window_d_y * window_d_z * input_data_list[0][0,0,0,:].size
+    inputs = np.empty((n_receptive_fields, receptive_field_size))
+    outputs = np.empty(n_receptive_fields)
+
+    index = 0
+
+
+    # Iterate through all images
+    for i in range(0, len(input_data_list)):
+
+        input_data = input_data_list[i]
+        output_data = output_data_list[i]
+
+        if (input_data[:,:,:,0].shape != output_data.shape):
+            raise ValueError('Input and output do not have the same shape.', input_data[:,:,:,0].shape, output_data.shape)
+
+        n_x, n_y, n_z, n_c = input_data.shape
+        n_voxels_per_subject = n_x * n_y * n_z
+
+        # pad the image to allow for an receptive field even at the borders
+        padding = max([rf_x, rf_y, rf_z])
+        padded_input_data = pad(input_data, padding)
+
+        # Create patches centered on all the voxels in the image
+        # (do not use the 4th dimension for patches, as this is the perfusion parameter channel)
+        input_fields = rolling_window(padded_input_data, (window_d_x, window_d_y, window_d_z, 0))
+        # Reshape to linear input
+        linear_input_fields = input_fields.reshape((n_voxels_per_subject, n_c))
+        inputs[index : index + n_voxels_per_subject] = linear_input_fields
+
+        # Reshape to linear output
+        linear_output = output_data.reshape(n_voxels_per_subject)
+        outputs[index : index + n_voxels_per_subject] = linear_output
+
+        index += n_voxels_per_subject
+    # End for (loop through subjects)
+
+    print('Entire dataset. Input shape: ', inputs.shape,
+          ' and output shape: ', outputs.shape)
+
+    return inputs, outputs
+
 
 def new_reshape_to_receptive_field(input_data_list, output_data_list, receptive_field_dimensions) :
     # Dimensions of the receptive field defined as distance to center point in every direction
@@ -46,6 +102,8 @@ def new_reshape_to_receptive_field(input_data_list, output_data_list, receptive_
 
         input_fields = rolling_window(padded_input_data, (window_d_x, window_d_y, window_d_z, 0))
 
+        # TODO: try returning the whole flattened array?
+
         for x, y, z in itertools.product(range(n_x),
                                          range(n_y),
                                          range(n_z)):
@@ -59,6 +117,14 @@ def new_reshape_to_receptive_field(input_data_list, output_data_list, receptive_
             output[index] = output_voxel
             index += 1
             # output.append(output_voxel)
+
+        lin_reshape = input_fields.reshape((n_x * n_y * n_z, n_c))
+        lin_reshape_out = output_data.reshape(n_x * n_y * n_z)
+        print('ooooooooooooo', lin_reshape.shape, inputs.shape, np.array_equal(lin_reshape, inputs))
+        print('iiiiiiiii', lin_reshape_out.shape, lin_reshape_out.shape, np.array_equal(lin_reshape_out, output))
+
+
+
 
     # inputs = np.squeeze(inputs)
     # output = np.squeeze(output)
