5f1d336f336057b6d961765145c8c04485e60eba
monsieurwave <tensu.wave@gmail.com>
2018-08-03 11:49:00 +0200

    Added capabilty of plotting ROC AUC with regards to rf

--- analysis/voxelWise/scoring_utils.py
+++ analysis/voxelWise/scoring_utils.py
@@ -1,7 +1,62 @@
+import os, torch
 import matplotlib.pyplot as plt
 from sklearn.metrics import f1_score, accuracy_score, fbeta_score, jaccard_similarity_score, roc_auc_score, precision_score, roc_curve, auc, accuracy_score
 import numpy as np
 
+
+def plot_auc_roc(rf_dims, roc_auc_scores):
+    """
+    Plot roc_auc for each value of rf (receptive field dimension)
+
+    Args:
+        rf_dims: list of receptiveField dimensions
+        roc_auc_scores: list of roc_auc_scores coresponding to the rf_dim at the same index
+
+    Returns:
+        undefined
+    """
+
+    mean_roc_auc_scores = []
+    mean_rf_dims = []
+
+    # print(rf_dims, roc_auc_scores)
+    roc_auc_scores = [x for _,x in sorted(zip(rf_dims, roc_auc_scores))]
+    rf_dims.sort()
+
+    for i in range(len(rf_dims)):
+
+        if len(roc_auc_scores[i]) != 0:
+            mean_roc_auc_scores.append( sum(roc_auc_scores[i]) / float(len(roc_auc_scores[i])) )
+            mean_rf_dims.append(rf_dims[i])
+
+        for j in range(len(roc_auc_scores[i])):
+            print(rf_dims[i], roc_auc_scores[i][j])
+            plt.plot(rf_dims[i], roc_auc_scores[i][j], 'k.', lw=1, alpha=0.3)
+
+    plt.plot(mean_rf_dims, mean_roc_auc_scores)
+
+    plt.ylim([-0.05, 1.05])
+    # plt.xlim([-0.05, 5.05])
+    plt.ylabel('ROC AUC')
+    plt.xlabel('Receptive field size (as voxels from center)')
+    plt.title('Area under the ROC curve')
+    # plt.legend(loc="lower right")
+
+    plt.ion()
+    plt.draw()
+    plt.show()
+
+def wrapper_plot_auc_roc(score_dir):
+    roc_auc_scores = []
+    rf_dims = []
+    score_paths = []
+    files = os.listdir(score_dir)
+    for file in files:
+        if (file.startswith('scores_rf_hyperopt_')):
+            score_path = os.path.join(score_dir, file)
+            rf_dims.append(file.split('_')[-1].split('.')[0])
+            roc_auc_scores.append(torch.load(score_path)['test_roc_auc'])
+    plot_auc_roc(rf_dims, roc_auc_scores)
 
 def plot_roc(tprs, fprs):
     """
