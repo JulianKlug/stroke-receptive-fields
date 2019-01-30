efd55f85e0e96d4143482305feca6c2525aee909
monsieurwave <tensu.wave@gmail.com>
2019-01-30 17:54:04 +0100

    Support for receptive fields with the Tmax6 threshold model - all voxels in the receptive field are thresholded and are then combinend in a log-GLM

--- analysis/voxelWise/vxl_threshold/Tmax6.py
+++ analysis/voxelWise/vxl_threshold/Tmax6.py
@@ -1,21 +1,38 @@
 import numpy as np
+from sklearn import linear_model
+
 from vxl_threshold.Treshold_Model import Treshold_Model
 
 class Tmax6_treshold():
-    def __init__(self):
+    def __init__(self, rf):
+        self.rf = np.max(rf)
         self.threshold = 6
+        self.combinator = linear_model.LogisticRegression(verbose = 0, max_iter = 1000000000, n_jobs = -1)
 
     def fit(self, X_train, y_train):
+        if self.rf != 0:
+            tresholded_voxels = np.zeros(X_train.shape)
+            tresholded_voxels[X_train > self.threshold] = 1
+            self.combinator.fit(tresholded_voxels, y_train)
         return self
 
+
     def predict_proba(self, data):
-        output = np.zeros(data.shape)
-        output[data > self.threshold] = 1
-        return np.squeeze(output)
+        tresholded_voxels = np.zeros(data.shape)
+        tresholded_voxels[data > self.threshold] = 1
+        if self.rf == 0:
+            # Simple Treshold case
+            return np.squeeze(tresholded_voxels)
+        else:
+            # log-linear combination of thresholded voxels in receptiveField
+            probas_ = self.combinator.predict_proba(tresholded_voxels)
+            return probas_[:, 1]
 
 class Tmax6(Treshold_Model):
-    def __init__(self, fold_dir, fold_name, n_channels = 4, n_channels_out = 1, rf = 1):
-        super().__init__(fold_dir, fold_name, model = Tmax6_treshold())
+    def __init__(self, fold_dir, fold_name, n_channels = 1, n_channels_out = 1, rf = 1):
+        super().__init__(fold_dir, fold_name, model = Tmax6_treshold(rf))
+        if (n_channels != 1):
+            raise Exception('Tmax6 Treshold model only works with one channel (Preferably Tmax).')
 
     @staticmethod
     def hello_world():
