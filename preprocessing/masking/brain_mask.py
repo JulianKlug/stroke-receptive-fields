47edc67bc46bd316f6e5f7bfc32342b77ab36dcd
monsieurwave <tensu.wave@gmail.com>
2019-05-06 00:16:28 +0200

    Apply csf mask to brain mask

--- preprocessing/masking/brain_mask.py
+++ preprocessing/masking/brain_mask.py
@@ -16,64 +16,7 @@ def getMask(path):
     labeled = labeled.astype(int)
     return labeled
 
-def createCSFMask(data_dir, spc_name, brain_labels):
-    """
-    mean and standard deviation were determined of the intensity histogram within [0,400] HU.
-    An upper intensity threshold μ − 1.96σ was applied to segment CSF
-    followed by erosion with a structuring element with 1 voxel radius to remove potential false positive voxels due to noise,
-    and connected region growing using the same thresholds.
-    The CSF mask was subjected to a morphological closing operation with a structuring element with 1 voxel radius.
-    """
-    # METHOD 1: according to scientific reports paper
-    path = os.path.join(data_dir, spc_name)
-    img = nib.load(path)
-    data = img.get_data()
-    data[np.isnan(data)] = 0
-    data[data < 3.8] = 0
-    clipped_range_data = data[(data > 0) & (data < 40)]
-    # clipped_range_data = data[(data > 0) & (data < 40) & (brain_labels == 1)]
-    # intensity threshold μ − 1.96σ
-    print(np.mean(clipped_range_data), np.std(clipped_range_data))
-    CSF_threshold = np.mean(clipped_range_data) - 1.96 * np.std(clipped_range_data)
-    # print(np.mean(clipped_range_data), np.std(clipped_range_data))
-    CSF_mask = np.zeros(data.shape)
-
-    print(CSF_threshold)
-    CSF_mask[(data < CSF_threshold)] = 1
-    # CSF_mask[(brain_labels == 1) & (data < CSF_threshold)] = 1
-
-    CSF_mask = binary_closing(CSF_mask)
-
-    CSF_mask = binary_erosion(CSF_mask)
-    CSF_mask = binary_dilation(CSF_mask)
-
-    # with fsl fast t1 5 class segmentation on spc
-    # fsl_path = '/usr/local/fsl/bin/'
-    out_dir = os.path.join(data_dir, 'fast_5seg')
-    if not os.path.exists(out_dir): os.mkdir(out_dir)
-    # coordinate_space = img.affine
-    # image_extension = '.nii'
-    # betted = np.zeros(data.shape)
-    # betted[brain_labels == 1] = data[brain_labels == 1]
-    # betted_img = nib.Nifti1Image(betted, affine=coordinate_space)
-    # betted_path = os.path.join(out_dir,  'betted_spc' + image_extension)
-    # nib.save(betted_img, betted_path)
-
-    # subprocess.call(['fast', '-t', '1', '-n', '5', '-H', '0.1', '-I', '4', '-l', '20.0',
-    #                     '-o', os.path.join(out_dir, spc_name), spc_name], cwd = data_dir)
-    # csf_class_proba_img = nib.load(os.path.join(out_dir, spc_name[:-4] + '_pve_2.nii.gz'))
-    # csf_class_proba_data = csf_class_proba_img.get_data()
-    # CSF_mask = np.zeros(csf_class_proba_data.shape)
-    # CSF_mask[csf_class_proba_data > 0.3] = 1
-    # CSF_mask = binary_erosion(CSF_mask)
-    # CSF_mask = binary_dilation(CSF_mask)
-    # CSF_mask = binary_closing(CSF_mask)
-
-
-    return CSF_mask
-
-
-def createBrainMask(data_dir, ct_sequences, spc_name, save = True):
+def createBrainMask(data_dir, ct_sequences, csf_image_name, save = True):
     """
     Create a brain mask according to RAPID maps (and save it)
 
@@ -98,9 +41,12 @@ def createBrainMask(data_dir, ct_sequences, spc_name, save = True):
 
     labeled = combined_labels.astype(int)
 
-    print(spc_name, len(channel_masks), combined_labels.shape)
-    csf = createCSFMask(data_dir, spc_name, labeled)
-    # labeled[csf == 1] = 0
+    csf_data = nib.load(os.path.join(data_dir, csf_image_name)).get_data()
+    csf_label = np.zeros(csf_data.shape)
+    csf_label[csf_data > 0.5] = 1
+
+    # substract csf_label to brain label
+    labeled[csf_label == 1] = 0
 
     inverse_labeled = 1 - labeled
 
@@ -113,14 +59,10 @@ def createBrainMask(data_dir, ct_sequences, spc_name, save = True):
         nib.save(labeled_img, os.path.join(data_dir,  'brain_mask' + image_extension))
         inverse_labeled_img = nib.Nifti1Image(inverse_labeled, affine=coordinate_space)
         nib.save(inverse_labeled_img, os.path.join(data_dir,  'inverse_brain_mask' + image_extension))
-
-        csf_img = nib.Nifti1Image(csf.astype(int), affine=coordinate_space)
-        nib.save(csf_img, os.path.join(data_dir,  'betted_csf_mask' + image_extension))
-
-    return labeled, csf
+    return labeled, csf_label
 
 def createBrainMaskWrapper(data_dir):
-    spc_base = 'wbetted_SPC_301mm_Std'
+    csf_image_name = 'wCSF_mask.nii'
     ct_sequences = ['wcoreg_Tmax', 'wcoreg_MTT', 'wcoreg_CBV', 'wcoreg_CBF']
     # ct_sequences = ['wcoreg_RAPID_Tmax', 'wcoreg_RAPID_MTT', 'wcoreg_RAPID_rCBV', 'wcoreg_RAPID_rCBF']
     # ct_sequences = ['wcoreg_RAPID_TMax', 'wcoreg_RAPID_MTT', 'wcoreg_RAPID_CBV', 'wcoreg_RAPID_CBF']
@@ -138,7 +80,5 @@ def createBrainMaskWrapper(data_dir):
             for modality in modalities:
                 if modality.startswith('Ct') or modality.startswith('pCT'):
                     modality_dir = os.path.join(subject_dir, modality)
-                    spc_name = spc_base + '_' + subject + '.nii'
-                    labeled, csf = createBrainMask(modality_dir, ct_sequences, spc_name)
+                    createBrainMask(modality_dir, ct_sequences, csf_image_name)
                     print('Processed subject:', subject)
-    return labeled, csf
