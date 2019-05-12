951a98f33a358a849717b42e1e1bc77dc2a3f96d
MonsieurWave <tensu.wave@gmail.com>
2019-05-12 10:50:32 +0200

    updated CSF segmentation description

--- preprocessing/skull_strip/csf_segmentation.py
+++ preprocessing/skull_strip/csf_segmentation.py
@@ -5,11 +5,17 @@ from scipy.ndimage.morphology import binary_closing, binary_erosion, binary_dila
 
 def createCSFMask(data_dir, betted_spc_name):
     """
-    mean and standard deviation were determined of the intensity histogram within [0,400] HU.
-    An upper intensity threshold μ − 1.96σ was applied to segment CSF
+    Input: 
+    - betted_spc_name: name of the original native CT file (SPC) that has been skull-stripped 
+    (the SPC file should not be coregistered or normalised as this slightly alters thesholds)
+    - data_dir : directory in which to find the native CT file
+    
+    Mean and standard deviation are determined of the intensity histogram within [0,400] HU.
+    An upper intensity threshold μ − 1.96σ is applied to segment CSF
     followed by erosion with a structuring element with 1 voxel radius to remove potential false positive voxels due to noise,
     and connected region growing using the same thresholds.
-    The CSF mask was subjected to a morphological closing operation with a structuring element with 1 voxel radius.
+    The CSF mask was subjected to a morphological closing operation with a structuring element with 2 voxel radius.
+    Inspired by: Manniesing R, Oei MTH, Oostveen LJ, Melendez J, Smit EJ, Platel B, et al. White Matter and Gray Matter Segmentation in 4D Computed Tomography. Scientific Reports. 2017 Mar 9;7(1):119.
     """
     path = os.path.join(data_dir, betted_spc_name)
     img = nib.load(path)
