85ef32f8ca6c5e039024013b8c57218504eaf782
monsieurwave <tensu.wave@gmail.com>
2019-10-09 18:55:51 +0200

    Added util to transform dicom to jpg

---
+++ preprocessing/utils/dictom_to_jpg.py
@@ -0,0 +1,22 @@
+import pydicom as dicom
+import os
+import cv2
+import PIL # optional
+# make it True if you want in PNG format
+PNG = False
+# Specify the .dcm folder path
+folder_path = "/Users/julian/temp/RAPID_TMax_[s]"
+# Specify the output jpg/png folder path
+jpg_folder_path = "/Users/julian/temp/JPG_test"
+images_path = os.listdir(folder_path)
+for n, image in enumerate(images_path):
+    print(n)
+    ds = dicom.dcmread(os.path.join(folder_path, image))
+    pixel_array_numpy = ds.pixel_array
+    if PNG == False:
+        image = image.replace('.dcm', '.jpg')
+    else:
+        image = image.replace('.dcm', '.png')
+    cv2.imwrite(os.path.join(jpg_folder_path, image), pixel_array_numpy)
+    if n % 50 == 0:
+        print('{} image converted'.format(n))
