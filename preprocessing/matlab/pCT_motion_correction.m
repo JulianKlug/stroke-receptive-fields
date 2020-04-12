3c929de4da4b5cd67eb0983f3cd01ee768079d9d
Julian Heinrich Klug <klug@miplabsrv2.epfl.ch>
2020-04-12 21:00:04 +0200

    Skip non subject folders in motion correction

--- preprocessing/matlab/pCT_motion_correction.m
+++ preprocessing/matlab/pCT_motion_correction.m
@@ -8,14 +8,14 @@
 clear all , clc
 %% Specify paths
 % Main folder containing individual subject folders
-data_path = '/Users/julian/temp/VPCT_extraction_test/pipeline_test_compa';
+data_path = '/home/klug/data/original_data/with_pct_2016_2017';
 
 % Options
 do_not_recalculate = true;
 
 % Prequisite paths
-DPABI_path = '/Users/julian/Documents/MATLAB/DPABI_V4.3_200301';
-spm_path = '/Users/julian/Documents/MATLAB/spm12';
+DPABI_path = '/home/klug/utils/DPABI_V4.3_200401';
+spm_path = '/home/klug/spm12';
 script_path = mfilename('fullpath');
 script_folder = script_path(1 : end - size(mfilename, 2));
 addpath(genpath(script_folder));
@@ -31,21 +31,23 @@ subjects(ismember(subjects,{'.','..'})) = [];
 
 % loop through subjects to process
 for i = 1: numel(subjects)
-    fprintf('%i/%i (%i%%) \n', i, size(subjects, 1), 100 * i / size(subjects, 1));
+    fprintf('%s %i/%i (%i%%) \n', subjects{i}, i, size(subjects, 1), 100 * i / size(subjects, 1));
 
     modalities = dir(fullfile(data_path,subjects{i}, 'pCT*'));
+    if length(modalities) < 1
+      fprintf('Skipping "%s" as pCT folder can not be found.\n', subjects{i});
+      continue;
+    end
     modality = modalities(1).name;
     
     CT_folder = fullfile(data_path, subjects{i}, modality);
     VPCT_file = dir(fullfile(CT_folder, ...
-                    strcat('VPCT_', subjects{i}, '*', '.nii*')));
-                
+                    strcat('VPCT_', subjects{i}, '.nii*')));
     % Check if subject already processed
     if do_not_recalculate && exist(fullfile(CT_folder, strcat('mc_', VPCT_file.name)))
         fprintf('Skipping subject "%s" as normalised files are already present.\n', subjects{i});
         continue;
     end
-    
     % Apply motion correction to this file
     apply_motion_correction(VPCT_file, CT_folder);
 end
