52d5327a0bcfab858b67f59c1d407a5e359ab548
Julian Klug <tensu.wave@gmail.com>
2020-04-20 11:20:51 +0200

    Delete test.py

--- preprocessing/pCT_preprocessing_pipeline/test.py
+++
@@ -1,7 +0,0 @@
-import nipype.interfaces.matlab as mlab
-import nipype.interfaces.spm as spm
-
-spm_path = '/home/klug/spm12'
-mlab.MatlabCommand.set_default_paths(spm_path)
-
-print(spm.SPMCommand().version)  
