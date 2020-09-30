e8c71dcd5ad66886354949474af0100e7f5d5096
Julian Klug <tensu.wave@gmail.com>
2020-09-30 10:47:35 +0200

    updated rf figures

--- analysis/voxelwise/figures/compare_ROC_AUC_boxplot_custom_order.py
+++ analysis/voxelwise/figures/compare_ROC_AUC_boxplot_custom_order.py
@@ -10,20 +10,8 @@ def flatten(l):
         return l
     return [item for sublist in l for item in sublist]
 
-def boxplot_sorted(df, by, column, rot=0, ax=None, hue=None):
-    # use dict comprehension to create new dataframe from the iterable groupby object
-    # each group name becomes a column in the new dataframe
-    df2 = pd.DataFrame({col:vals[column] for col, vals in df.groupby(by)})
-    # find and sort the median values in this new dataframe
-    meds = df2.median().sort_values()
-    # use the columns in the dataframe, ordered sorted by median value
-    # return axes so changes can be made outside the function
-    # return df2[meds.index].boxplot(rot=rot, hue=hue, return_type="axes", ax = ax)
-    return sns.boxplot(x = 'model_tag', y = column, order = meds.index, data = df, hue = hue,
-        ax = ax, dodge = False, palette="Pastel2")
-
 columns = ['model_name', 'rf', 'model_class', 'model_tag', 'roc_auc', 'dice']
-def wrapper_plot_auc_roc_boxplot(modality_dirs, save_dir):
+def wrapper_plot_auc_roc_boxplot(modality_dirs, save_dir, order):
     scores_df = pd.DataFrame(columns = columns)
 
     for modality_dir in modality_dirs[:]:
@@ -54,7 +42,7 @@ def wrapper_plot_auc_roc_boxplot(modality_dirs, save_dir):
                     rf = int(result_files[0].split('_')[-1].split('.')[0])
 
                 if 'all_pCT' in model_name:
-                    model_class = 'glm(multi)'
+                    model_class = 'glm(all channels)'
                 if 'Tmax' in model_name:
                     model_class = 'glm(Tmax)'
                 if 'Tmax0_6' in model_name:
@@ -99,41 +87,67 @@ def wrapper_plot_auc_roc_boxplot(modality_dirs, save_dir):
                 scores_df = scores_df.append(pd.DataFrame(new_entries, columns = columns))
 
 
-    fig, ax = plt.subplots()
-    axes = boxplot_sorted(scores_df, by=["model_tag"], column="roc_auc", hue='rf', ax = ax)
-    labels = [l.get_text() for l in axes.get_xticklabels()]
-    ax.set_xticklabels(labels, rotation=35, ha='right')
-    ax.set_title("Boxplots of the area under the ROC curve by model")
-    plt.ylabel('Area under the ROC curve')
-    plt.xlabel('')
-    # Receptive field size (as voxels from center)
-    fig.tight_layout()
-    fig1 = plt.gcf()
-    plt.show()
-    fig1.savefig(os.path.join(save_dir, "roc_auc_boxplot_all.svg"), format="svg")
+    # fig, ax = plt.subplots()
+    # g = sns.FacetGrid(scores_df, col='model_class')
+    # g.map(sns.boxplot, "model_tag", "roc_auc", hue="rf")
+    # g.add_legend()
+    # sns.set(font_scale=1.1)
+    g = sns.catplot(x="rf", y="roc_auc", hue="rf", col='model_class', dodge=False, aspect=0.6,
+                    data=scores_df, kind="box", legend=True, legend_out=True, palette="Pastel2",
+                    col_order=['glm(MTT)', 'glm(CBV)', 'glm(CBF)', 'glm(Tmax)', 'glm(all channels)'])
+    (g.set_axis_labels("", "Area under the ROC curve")
+     .set_xticklabels(["No receptive field", "Receptive field at rf 3"],
+                      rotation=35, ha='right')
+     .set_titles("{col_name}", size=16)
+    )
 
-    fig, ax = plt.subplots()
-    axes = boxplot_sorted(scores_df, by=["model_tag"], column="dice", hue='rf', ax=ax)
-    labels = [l.get_text() for l in axes.get_xticklabels()]
-    ax.set_xticklabels(labels, rotation=35, ha='right')
-    ax.set_title("Boxplots of Dice coefficients by model")
-    plt.ylabel('Sørensen–Dice coefficient')
-    plt.xlabel('')
-    # Receptive field size (as voxels from center)
-    fig.tight_layout()
-    fig1 = plt.gcf()
+    g.fig.subplots_adjust(top=0.88)
+    plt.suptitle('Boxplots of the area under the ROC curve by model', y=1.2)
+    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0., fancybox=True, title="rf")
     plt.show()
-    fig1.savefig(os.path.join(save_dir, "dice_boxplot_all.svg"), format="svg")
+    g.fig.savefig(os.path.join(save_dir, "roc_auc_boxplot_per_model.svg"), format="svg")
 
-main_dir = '/Users/julian/temp/boxplot_figure'
+    # g.add_legend()
+    # axes = sns.boxplot(x='model_tag', y="roc_auc", data=scores_df, hue='rf', order=order,
+    #                    ax=ax, dodge=False, palette="Pastel2")
+    # labels = [l.get_text() for l in axes.get_xticklabels()]
+    # ax.set_xticklabels(labels, rotation=35, ha='right')
+    # ax.set_title("Boxplots of the area under the ROC curve by model")
+    # plt.ylabel('Area under the ROC curve')
+    # plt.xlabel('')
+    # Receptive field size (as voxels from center)
+    # fig.tight_layout()
+    # fig1 = plt.gcf()
+    # fig1.savefig(os.path.join(save_dir, "roc_auc_boxplot_all.svg"), format="svg")
+
+    # fig, ax = plt.subplots()
+    # # axes = sns.boxplot(x = 'model_tag', y="dice", order = order, data = scores_df, hue = 'rf',
+    # #                     ax = ax, dodge = False, palette="Pastel2")
+    # axes = sns.boxplot(x = 'model_tag', y="dice", data = scores_df, hue = 'rf',
+    #                     ax = ax, dodge = False, palette="Pastel2")
+    # labels = [l.get_text() for l in axes.get_xticklabels()]
+    # ax.set_xticklabels(labels, rotation=35, ha='right')
+    # ax.set_title("Boxplots of Dice coefficients by model")
+    # plt.ylabel('Sørensen–Dice coefficient')
+    # plt.xlabel('')
+    # # Receptive field size (as voxels from center)
+    # fig.tight_layout()
+    # fig1 = plt.gcf()
+    # plt.show()
+    # fig1.savefig(os.path.join(save_dir, "dice_boxplot_all.svg"), format="svg")
+
+main_dir = '/Users/julian/temp/selected_models_for_ESO_presentation'
 multiGLM = os.path.join(main_dir, 'all_pCT_logReg')
 MTT = os.path.join(main_dir, 'MTT2_logReg')
 Tmax = os.path.join(main_dir, 'Tmax0_logReg')
 CBF = os.path.join(main_dir, 'CBF1_logReg')
 CBV = os.path.join(main_dir, 'CBV3_logReg')
-# Tresh_Tmax = os.path.join(main_dir, 'Tmax_threshold')
-# Campbell_model = os.path.join(main_dir, 'Campbell_model')
-continuous_Tmax = os.path.join(main_dir, 'continuous_Tmax')
 
-# wrapper_plot_auc_roc_boxplot([Tresh_Tmax, Tmax])
-wrapper_plot_auc_roc_boxplot([multiGLM, MTT, Tmax, CBF, CBV, continuous_Tmax], main_dir)
+order = ['glm(MTT) at rf 0', 'glm(MTT) at rf 3',
+         'glm(CBV) at rf 0', 'glm(CBV) at rf 3',
+         'glm(CBF) at rf 0', 'glm(CBF) at rf 3',
+         'glm(Tmax) at rf 0', 'glm(Tmax) at rf 3',
+         'glm(multi) at rf 0', 'glm(multi) at rf 3',
+         ]
+
+wrapper_plot_auc_roc_boxplot([multiGLM, MTT, Tmax, CBF, CBV], main_dir, order)
